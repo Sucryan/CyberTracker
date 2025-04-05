16f3ce7f103b32595c7aca4aa32ce2625a2d3570
sucryan <a0987448538@gmail.com>
2025-04-05 18:29:04 +0800

    v2-4, 250405

--- build.bat
+++ build.bat
@@ -1,24 +1,32 @@
 @echo off
+:: 切換到程式所在的目錄
+cd /d "%~dp0"
+echo Current directory is: %CD%
+
 setlocal enabledelayedexpansion
 
-:: 變數設置
-set PYTHON_ENV=C:\Users\a0987\env
+:: 設定 Python 虛擬環境路徑
+set PYTHON_ENV=E:\env
+
+:: 定義 (exe檔名 : py檔名) 的對應清單
 set SCRIPTS=CyberTracker:UI.py web_capture:web_capture.py merge_csv:merge_csv.py csv_to_xlsx:csv_to_xlsx.py
 
-:: 確保 Python 環境可用
-if exist %PYTHON_ENV%\Scripts\activate (
+:: 檢查並啟用虛擬環境
+if exist "%PYTHON_ENV%\Scripts\activate" (
     echo starting venv
-    call %PYTHON_ENV%\Scripts\activate
+    call "%PYTHON_ENV%\Scripts\activate"
 ) else (
-    echo didn't find vevn, using global settting...
+    echo didn't find venv, using global setting...
 )
 
 :: 開始打包
 for %%A in (%SCRIPTS%) do (
     for /f "tokens=1,2 delims=:" %%B in ("%%A") do (
         echo packing %%C to %%B.exe...
-        pyinstaller --onefile --noconsole %%B %%C
-        if %errorlevel%==0 (
+        echo Running: pyinstaller --onefile --noconsole "%%C" --name "%%B" --log-level=DEBUG
+        pyinstaller --onefile --noconsole "%%C" --name "%%B" --log-level=DEBUG
+
+        if !errorlevel!==0 (
             echo %%B success
         ) else (
             echo %%B fail
